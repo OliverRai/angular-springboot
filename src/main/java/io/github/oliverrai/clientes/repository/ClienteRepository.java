6e4b769fccf6405574b31c03afc320a43db6ea5f
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-07-18 22:37:16 -0300

    adicionando ServicoRepository

---
+++ src/main/java/io/github/oliverrai/clientes/repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package io.github.oliverrai.clientes.repository;
+
+import io.github.oliverrai.clientes.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
