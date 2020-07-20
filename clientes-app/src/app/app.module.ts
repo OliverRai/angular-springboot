27d5801b5ee5132185918c7f510cba2039f2b84d
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-07-20 13:12:28 -0300

    adicionando navbar

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TemplateModule } from './template/TemplateModule'
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
