6fedb5afc4f2381271b8e007228afd14b1ad42db
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-07-20 14:29:08 -0300

    criando componente Home

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,15 +1,17 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TemplateModule } from './template/TemplateModule';
-
+import { TemplateModule } from './template/TemplateModule'
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
