fa506e1c0139754adef2f1f1888d3bc2b242eefa
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-07-20 15:51:44 -0300

    adicionando rota para Home

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 
 import { TemplateModule } from './template/TemplateModule'
 import { HomeComponent } from './home/home.component'
+import { ClientesModule } from './clientes/clientes.module'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TemplateModule
+    TemplateModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
