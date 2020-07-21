e87c7b1d09a3038fe99320767954a31a7e49c817
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-07-21 09:11:38 -0300

    adicionando service

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 import { TemplateModule } from './template/TemplateModule'
 import { HomeComponent } from './home/home.component'
 import { ClientesModule } from './clientes/clientes.module'
+import { ClientesService } from './clientes.service';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,14 @@ import { ClientesModule } from './clientes/clientes.module'
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule
   ],
-  providers: [],
+  providers: [
+    ClientesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -1,9 +1,20 @@
+import { Cliente } from './clientes/clientes-form/cliente';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor( private http : HttpClient ) {
+    
+   }
+
+  getCliente() : Cliente {
+    let cliente : Cliente = new Cliente();
+    cliente.nome = "Raiane";
+    cliente.cpf = "88888888888888888";
+    return cliente;
+  }
 }
