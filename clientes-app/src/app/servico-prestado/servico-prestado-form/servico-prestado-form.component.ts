0da6db161936a3f6f4c1238c50729be2bd536f48
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-08 13:21:38 -0300

    iniciando criação de servicos-form

--- clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './../../clientes/clientes-form/cliente';
+import { ClientesService } from '../../clientes.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoFormComponent implements OnInit {
 
-  constructor() { }
+  clientes : Cliente[] = [];
+
+  constructor(
+    private clienteService : ClientesService
+  ) { }
 
   ngOnInit(): void {
+    this.clienteService
+      .getClientes()
+      .subscribe( response => this.clientes = response)
+  }
+
+  onSubmit(){
+    console.log('submit');
   }
 
 }
--- clientes-app/src/app/servico-prestado/servico-prestado-routing.module.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'servico-form', component: ServicoPrestadoFormComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
