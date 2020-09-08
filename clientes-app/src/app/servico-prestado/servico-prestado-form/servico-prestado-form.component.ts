1c93469f5cfee3b52ca04bef109497767693585a
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-08 14:42:09 -0300

    formulário de servico concluído

--- clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,3 +1,4 @@
+import { ServicoPrestado } from './../servicoPrestado';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../clientes/cliente';
 import { ClientesService } from '../../clientes.service';
@@ -10,10 +11,13 @@ import { ClientesService } from '../../clientes.service';
 export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = [];
+  servico: ServicoPrestado;
 
   constructor(
     private clienteService : ClientesService
-  ) { }
+  ) {
+    this.servico = new ServicoPrestado();
+   }
 
   ngOnInit(): void {
     this.clienteService
@@ -22,7 +26,7 @@ export class ServicoPrestadoFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log('submit');
+    console.log(this.servico);
   }
 
 }
