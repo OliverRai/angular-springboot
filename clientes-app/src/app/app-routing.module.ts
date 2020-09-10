6b2890d3af5d895b7a79d7b34918d16e0c62c37c
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-10 15:13:58 -0300

    adicionando rotas filhas

--- clientes-app/src/app/app-routing.module.ts
+++ clientes-app/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
-  { path : 'home', component: HomeComponent },
-  {path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LayoutComponent, children: [
+    { path : 'home', component: HomeComponent },
+  ] }
 
 ];
 
