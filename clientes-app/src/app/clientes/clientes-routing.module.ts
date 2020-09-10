4100f2ccf6f5efe0488fc1cffbcf6f6fece070fa
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-10 15:46:15 -0300

    adicionando rotas filhas

--- clientes-app/src/app/clientes/clientes-routing.module.ts
+++ clientes-app/src/app/clientes/clientes-routing.module.ts
@@ -1,12 +1,18 @@
+import { LayoutComponent } from './../layout/layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component'
 import { ClientesListaComponent } from './clientes-lista/clientes-lista.component';
 
 const routes: Routes = [
-  {path: 'clientes-form', component: ClientesFormComponent},
-  {path: 'clientes-form/:id', component: ClientesFormComponent},
-  {path: 'clientes-lista', component: ClientesListaComponent}
+  { path: 'clientes', component: LayoutComponent, children: [
+
+    {path: 'form', component: ClientesFormComponent},
+    {path: 'form/:id', component: ClientesFormComponent},
+    {path: 'lista', component: ClientesListaComponent},
+    {path: '', redirectTo: '/clientes/lista', pathMatch: 'full' }
+  ]}
+  
 
 ];
 
