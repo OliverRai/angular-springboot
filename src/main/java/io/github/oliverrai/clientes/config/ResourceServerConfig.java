f05dd88add32694342829f930df3287330a04ac8
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-11 18:12:36 -0300

    Configurando o Resource Server

--- src/main/java/io/github/oliverrai/clientes/config/ResourceServerConfig.java
+++ src/main/java/io/github/oliverrai/clientes/config/ResourceServerConfig.java
@@ -1,4 +1,21 @@
 package io.github.oliverrai.clientes.config;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/api/usuarios").permitAll()
+                .antMatchers("/api/clientes/**", "/api/servicos-prestados/**").authenticated()
+            .anyRequest().denyAll();
+
+    }
 }
