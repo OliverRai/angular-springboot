b6a506f45b527c1f2be8b7c91953844490eea775
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-10 23:59:15 -0300

    adicionando dependencias spring security

---
+++ src/main/java/io/github/oliverrai/clientes/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package io.github.oliverrai.clientes.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+@EnableAuthorizationServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
