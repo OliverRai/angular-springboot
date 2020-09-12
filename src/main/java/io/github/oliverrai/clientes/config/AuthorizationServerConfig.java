ca047b6f066fd15f15893ead77ded2966870115b
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-11 22:22:47 -0300

    configurando OAuth para tokens JWT

--- src/main/java/io/github/oliverrai/clientes/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/oliverrai/clientes/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package io.github.oliverrai.clientes.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -18,15 +21,26 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${security.jwt.signing-key}")
+    private String signingKey;
+
     @Bean
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(signingKey);
+        return tokenConverter;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(authenticationManager);
     }
 
--- src/main/java/io/github/oliverrai/clientes/config/SecurityConfig.java
+++ src/main/java/io/github/oliverrai/clientes/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .userDetailsService(usuarioService)
+            .userDetailsService(usuarioService )
                 .passwordEncoder(passwordEncoder());
     }
 
