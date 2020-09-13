2385e9249ea2bf90027ec19050801fa41e3064c1
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-13 11:53:49 -0300

    guardando token na aplicação

--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -17,7 +17,12 @@ export class ClientesService {
    }
 
    salvar( cliente : Cliente) : Observable<Cliente>{
-      return this.http.post<Cliente>(`${this.apiURL}`, cliente)
+      const tokenString = localStorage.getItem('access_token');
+      const token = JSON.parse(tokenString);
+      const headers = {
+      'Authorization' : 'Bearer' + token.access_token
+    }
+      return this.http.post<Cliente>(`${this.apiURL}`, cliente, {headers})
    }
 
    atualizar( cliente : Cliente) : Observable<Cliente>{
@@ -29,7 +34,12 @@ export class ClientesService {
    }
 
    getClientes() : Observable<Cliente[]>{
-      return this.http.get<Cliente[]>(this.apiURL);
+     const tokenString = localStorage.getItem('access_token');
+     const token = JSON.parse(tokenString);
+     const headers = {
+       'Authorization' : 'Bearer' + token.access_token
+     }
+     return this.http.get<Cliente[]>(this.apiURL, {headers});
   }
 
   getClienteById(id : number) : Observable<Cliente>{
--- clientes-app/src/app/login/login.component.ts
+++ clientes-app/src/app/login/login.component.ts
@@ -26,6 +26,8 @@ export class LoginComponent  {
     this.authService
           .tentarLogar(this.username, this.password)
           .subscribe(response => {
+            const access_token = JSON.stringify(response);
+            localStorage.setItem('access_token', access_token)
             this.router.navigate(['/home'])
           }, errorResponse=>{
             this.errors = ["Usuário e/ou Senha incorreto(s)."]
