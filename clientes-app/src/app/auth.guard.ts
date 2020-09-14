3da7c254569962fa1e6fed7b16a68ff77df85438
oliverrai <raiane.mateus.oliveira@gmail.com>
2020-09-13 21:53:03 -0300

    controlando a sessão do usuário com o token

--- clientes-app/src/app/auth.guard.ts
+++ clientes-app/src/app/auth.guard.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
 
 
 @Injectable({
@@ -9,12 +10,21 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } fro
 export class AuthGuard implements CanActivate {
   
   constructor(
-    private authService : AuthService
+    private authService : AuthService,
+    private router : Router
   ){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean{
-      return this.authService.isAuthenticated();
+
+      const authenticated = this.authService.isAuthenticated();
+
+      if(authenticated){
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false
+      }
     }
   
 }
--- clientes-app/src/app/auth.service.ts
+++ clientes-app/src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { Usuario } from './login/usuario';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 import { environment } from '../environments/environment'
 
@@ -14,12 +15,27 @@ export class AuthService {
   tokenURL: string = environment.apiURLBase + environment.obterTokenURL;
   clientID: string = environment.clientId;
   clientSecret: string = environment.clientSecret; 
+  jwtHelper: JwtHelperService = new JwtHelperService;
 
   constructor(
     private http : HttpClient
   ) { }
+
+  obterToken(){
+    const tokenString = localStorage.getItem('access_token');
+    if(tokenString){
+      const token = JSON.parse(tokenString).access_token;
+      return token
+    }
+    return null;
+  }
  
   isAuthenticated() : boolean {
+    const token = this.obterToken();
+    if(token){
+      const expired = this.jwtHelper.isTokenExpired(token);
+      return !expired;
+    }
     return false;
   }
 
